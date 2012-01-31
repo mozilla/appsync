61d9dbc011dce36960b3d4cbb0dc48d775083626
Tarek Ziade <tarek@ziade.org>
2012-01-30 21:03:39 -0800

    tagging 0.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = ['cornice', 'mozsvc', 'WebTest', 'simplejson', 'SQLAlchemy',
 
 
 setup(name='appsync',
-      version='0.4',
+      version='0.5',
       description='appsync',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
